ec842dfd06fb1e72eb804e737679d7c3da7431d7
Costanza Assef <81304267+Costanza22@users.noreply.github.com>
2023-06-26 11:42:14 -0300

    Add files via upload

--- N3Seguranca/GetKeysEncrypt.py
+++ N3Seguranca/GetKeysEncrypt.py
@@ -17,29 +17,23 @@ def encripta(chave_file, entrada_file, saida_file):
     with open(entrada_file, "r") as entrada_arq:
         dado = entrada_arq.read().strip()
 
-    # Encripta o dado usando a chave
     encriptado = [str(pow(ord(char), expoente, modulo)) for char in dado]
 
-    # Escreve o resultado no arquivo de saída
     with open(saida_file, "w") as saida_arq:
         saida_arq.write("\n".join(encriptado))
 
 
 def decripta(chave_file, entrada_file, saida_file):
-    # Lê a chave do arquivo
     with open(chave_file, "r") as chave_arq:
         linhas = chave_arq.readlines()
         modulo = int(linhas[0].strip())
         expoente = int(linhas[1].strip())
 
-    # Lê os números encriptados do arquivo de entrada
     with open(entrada_file, "r") as entrada_arq:
         encriptado = [int(line.strip()) for line in entrada_arq.readlines()]
 
-    # Decripta os números usando a chave
     decriptado = [chr(pow(num, expoente, modulo)) for num in encriptado]
 
-    # Escreve o resultado no arquivo de saída
     with open(saida_file, "w") as saida_arq:
         saida_arq.write("".join(decriptado))
 
--- N3Seguranca/decripta.py
+++ N3Seguranca/decripta.py
@@ -1,20 +1,16 @@
 import sys
 
 def decripta(chave_file, entrada_file, saida_file):
-    # Lê a chave do arquivo
     with open(chave_file, "r") as chave_arq:
         linhas = chave_arq.readlines()
         modulo = int(linhas[0].strip())
         expoente = int(linhas[1].strip())
 
-    # Lê os números encriptados do arquivo de entrada
     with open(entrada_file, "r") as entrada_arq:
         encriptado = [int(line.strip()) for line in entrada_arq.readlines()]
 
-    # Decripta os números usando a chave
     decriptado = [chr(pow(num, expoente, modulo)) for num in encriptado]
 
-    # Escreve o resultado no arquivo de saída
     with open(saida_file, "w") as saida_arq:
         saida_arq.write("".join(decriptado))
 
